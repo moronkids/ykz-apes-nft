0444f047f2491a26844c331cd355da3cf732b645
Sulistyo <sulistyoardani@gmail.com>
2021-12-11 18:29:34 +0700

    fix: boxshadoew unset

--- src/components/Transactions/Home.tsx
+++ src/components/Transactions/Home.tsx
@@ -32,7 +32,7 @@ padding: unset !important;
     box-shadow: unset !important;
     border-radius: 20px !important;
     span:hover{
-      box-shadow: 0px 3px 1px -2px rgb(0 0 0 / 20%), 0px 2px 2px 0px rgb(0 0 0 / 14%), 0px 1px 5px 0px rgb(0 0 0 / 12%);
+      box-shadow: unset !important;
       border-radius: 10px !important
     }
     `
@@ -106,7 +106,7 @@ background: unset !important;
 const MintButton = styled(Button)`
 background: unset !important;
     span:hover {
-      box-shadow: 0px 3px 1px -2px rgb(0 0 0 / 20%), 0px 2px 2px 0px rgb(0 0 0 / 14%), 0px 1px 5px 0px rgb(0 0 0 / 12%);
+      box-shadow: unset !important;
       border-radius: 10px !important
     }
 `; // add your styles here
